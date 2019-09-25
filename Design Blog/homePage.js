07e1d16564671018b5caa663d8a77cf480904d6a
Debreczeni Andrea <d.andrea02@yahoo.com>
2019-09-25 17:38:31 +0300

    Design Blog Project created

---
+++ Design Blog/homePage.js
@@ -0,0 +1,4 @@
+var text = "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.\n" +
+    "    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n" +
+    "    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+document.getElementById("textJs").textContent = text;
